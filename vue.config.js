562d3592b3b5e5ebd70415bbfd065ded1065209e
alerjr <alessandro.rocjr@gmail.com>
2025-01-24 21:08:49 -0300

    Atualizando o projeto para o deploy no GitHub Pages

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/appReview/'  // Substitua 'nome-do-repositorio' pelo nome do seu repositório no GitHub
+        : '/'
+};
